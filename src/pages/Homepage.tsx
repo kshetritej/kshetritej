12ce2c7e9fd79a38799ff0c2bbcded404a780c83
kshetritej <tejghartikshetri@gmail.com>
2024-12-25 19:27:05 +0545

    BREAKING CHANGE: removes everythig expect navbar, rebuild in process

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,17 +1,8 @@
-import About from "@/components/About"
-import Experience from "@/components/Experience"
-import Header from "@/components/Header"
-import Projects from "@/components/Projects"
-import Skills from "@/components/Skills"
 
 const Homepage = () => {
   return (
     <>
-        <Header/>
-        <Projects/>
-        <Skills/>
-        <Experience/>
-        <About/>
+    hello
     </>
   )
 }
