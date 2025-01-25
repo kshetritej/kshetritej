cb7d72a8c7fbaf5e4fe6d67d85c9212fe36b7590
kshetritej <tejghartikshetri@gmail.com>
2025-01-25 12:41:43 +0545

    add: gtag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Image from "next/image";
-import Head from "next/head";
+import Script from "next/script";
 
 
 
@@ -18,8 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <script id="meta-pixel">
+      <head>
+
+        <Script id="google-tag-manager">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5QCV2H8D');`}</Script>
+
+        <Script id="meta-pixel">
           {`
           !function(f,b,e,v,n,t,s)
           {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
@@ -32,16 +39,17 @@ export default function RootLayout({
           fbq('init', '867342538733412');
           fbq('track', 'PageView');
           `}
-        </script>
+        </Script>
+      </head>
+      <body
+      >
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QCV2H8D"
+          height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
         <noscript>
           <Image height="1" width="1" style={{ display: "none" }} alt="alternative image for facebook pixel"
             src="https://www.facebook.com/tr?id=867342538733412&ev=PageView&noscript=1"
           />
         </noscript>
-
-      </Head>
-      <body
-      >
         <ThemeProvider attribute="class">
           {children}
         </ThemeProvider>
