7a03da52bdaa9a1fbe44fc35a71ecbd3bbbf810e
kshetritej <tejghartikshetri@gmail.com>
2025-01-25 13:18:42 +0545

    add: i18n

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
             },
         ],
     },
+    i18n:{
+        locales: ['en','ne', 'es', 'fr'],
+        defaultLocale: 'en'
+    }
 };
 
 export default nextConfig;
