97bc08ddca81df30af47d4836565c3a9f53a20c1
kshetritej <tejghartikshetri@gmail.com>
2025-01-25 13:15:00 +0545

    add: meta tags and sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import Script from "next/script";
 
 
 export const metadata: Metadata = {
-  title: "Tej Kshetri",
-  description: "Fullstack Web Developer",
+  title: "Tej Kshetri - Fullstack Web Developer",
+  description: "Hello! I am Tej, Fullstack Developer based in Pokhara, Nepal. I offer both web and software development services to people all around the globe. Starting from 2021 I have gathered knowledge and expertise in design, engineering, and support services. Along with me my team of skilled professionals brings creative ideas to life and delivers exceptional results, making sure that clients stay ahead in the digital landscape.We consistently provide innovative and exceptional customer service, making them the go-to partner for all things in tech.I",
 };
 
 export default function RootLayout({
@@ -18,12 +18,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-
         <Script id="google-tag-manager">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-5QCV2H8D');`}</Script>
+        <meta charSet="UTF_8" />
+        <meta name="keywords" content="fullstack, web development, reactjs, nodejs, typescript, website design, web application development, web developer in pokhara"/>
+        <meta name="robots" content="index, follow"/>
+        <meta property="og:title" content="Tej Kshetri - Fullstack Web Developer | MERN | Web Designer"/>
+        <meta property="og:description" content="Hello! I am Tej, Fullstack Developer based in Pokhara, Nepal. I offer both web and software development services to people all around the globe. Starting from 2021 I have gathered knowledge and expertise in design, engineering, and support services. Along with me my team of skilled professionals brings creative ideas to life and delivers exceptional results, making sure that clients stay ahead in the digital landscape.We consistently provide innovative and exceptional customer service, making them the go-to partner for all things in tech."/>
+        <meta property="og:url" content="https://kshetritej.com.np/"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:image" content="/og-image.png"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="canonical" href="https://kshetritej.com.np"/>
       </head>
       <body
       >
---
+++ next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: 'https://kshetritej.com.np',
+  generateRobotsTxt: true,
+};
